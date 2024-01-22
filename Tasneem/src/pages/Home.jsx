d6e16a0b0956db02107b7a18ac96d72c307045ff
tasalluhimi <tasneem.luhimi@gmail.com>
2024-01-22 13:31:30 +0300

    test for 3d thing c:

---
+++ Tasneem/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline'
+
+function Home() {
+  return (
+    <>
+      <div className='h-screen'>
+        <Spline scene='https://prod.spline.design/f4wzBN5IlHNoq4jp/scene.splinecode'></Spline>
+      </div>
+    </>
+  )
+}
+
+export default Home
